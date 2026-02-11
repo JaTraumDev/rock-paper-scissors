e740e8fc142de72a9c2955557d5e93b6d57b861f
JaTraumDev <74469317+JaTraumDev@users.noreply.github.com>
2026-02-11 15:26:01 +0100

    feat: add getComputerChoice() function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function getComputerChoice(){
+    random = Math.random();
+    console.log(random);
+    if(random < 1/3){
+        return "Rock";
+    } else if(random > 1/3 && random < 2/3){
+        return "Paper";
+    } else {
+        return "Scissors"
+    }
+}
+
 
